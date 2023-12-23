bd84c245eda7ccb6463b5e14be365099b68e93f0
Yafet Daniel <132993144+JaphD@users.noreply.github.com>
2023-12-23 18:19:32 +0300

    Update InstructorLoginPage.java

--- EduTrack/src/GUI/InstructorLoginPage.java
+++ EduTrack/src/GUI/InstructorLoginPage.java
@@ -97,7 +97,7 @@ public class InstructorLoginPage extends Page implements ActionListener {
         if (e.getSource() == signUpButton) {
             try {
                 new InstructorSignUpPage();
-                dispose(); // Close current window after opening new page
+                frame.dispose(); // Close current window after opening new page
             } catch (Exception ex) {
                 // Handle error opening StudentLoginPage
                 JOptionPane.showMessageDialog(this, "Error opening Student Login: " + ex.getMessage());
