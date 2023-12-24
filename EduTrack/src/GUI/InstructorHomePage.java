04afa3a41a2513212261eb9ad4f1e43749f80525
Yafet Daniel <132993144+JaphD@users.noreply.github.com>
2023-12-24 20:31:46 +0300

    Update InstructorHomePage.java

--- EduTrack/src/GUI/InstructorHomePage.java
+++ EduTrack/src/GUI/InstructorHomePage.java
@@ -10,31 +10,54 @@ public class InstructorHomePage extends Page {
 
     public InstructorHomePage() {
         super("EduTrack - Instructor Homepage",211, 211, 211);
-
+        this.setTitle("EduTrack - Instructor Homepage");
         this.instructorMenuBar = new JMenuBar();
 
         JMenu homeMenu = new JMenu("Home");
         JMenu menuMenu = new JMenu("Menu");
-        JMenu logoutMenu = new JMenu("Logout");
 
-        JMenuItem profileItem = new JMenuItem("Profile");
+        JMenuItem accountItem = new JMenuItem("Account");
         JMenuItem assessmentItem = new JMenuItem("Assessment");
-        JMenuItem attendanceItem = new JMenuItem("Course Material");
+        JMenuItem attendanceItem = new JMenuItem("Attendance");
+        JMenuItem courseItem = new JMenuItem("Course Material");
         JMenuItem quizRoomItem = new JMenuItem("Quiz Room");
+        JMenuItem contactItem = new JMenuItem("Contact");
+        JMenuItem logoutItem = new JMenuItem("Logout");
 
-        formatMenuItem(profileItem);
+        accountItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Handle logout button click event
+                account();
+            }
+        });
+
+        logoutItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Handle logout button click event
+                logout();
+            }
+        });
+
+        formatMenuItem(accountItem);
         formatMenuItem(assessmentItem);
         formatMenuItem(attendanceItem);
+        formatMenuItem(courseItem);
         formatMenuItem(quizRoomItem);
+        formatMenuItem(contactItem);
+        formatMenuItem(logoutItem);
 
-        menuMenu.add(profileItem);
+        menuMenu.add(accountItem);
         menuMenu.add(assessmentItem);
         menuMenu.add(attendanceItem);
+        menuMenu.add(courseItem);
         menuMenu.add(quizRoomItem);
+        menuMenu.add(contactItem);
+        menuMenu.add(logoutItem);
 
         instructorMenuBar.add(homeMenu);
         instructorMenuBar.add(menuMenu);
-        instructorMenuBar.add(logoutMenu);
 
         // Create panels for each button and set layout
         JPanel announcementPanel = createPanel("Announcement");
@@ -47,14 +70,67 @@ public class InstructorHomePage extends Page {
         this.add(studentListPanel);
         this.add(assignmentPanel);
 
-        ImageIcon icon = new ImageIcon("C:\\Users\\Japhe\\OneDrive\\Desktop\\Java\\EduTrack\\src\\GUI\\icon2.jpeg");
-        this.setIconImage(icon.getImage());
+        ImageIcon icon = createImageIcon("icon2.jpeg");
+        if (icon != null) {
+            this.setIconImage(icon.getImage());
+        }
 
         this.setJMenuBar(instructorMenuBar);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
     }
+    InstructorHomePage(String title){
+        super("EduTrack - Instructor Homepage",211, 211, 211);
+        this.setTitle(title);
+        this.instructorMenuBar = new JMenuBar();
 
+        JMenu homeMenu = new JMenu("Home");
+        JMenu menuMenu = new JMenu("Menu");
+
+        JMenuItem accountItem = new JMenuItem("Account");
+        JMenuItem assessmentItem = new JMenuItem("Assessment");
+        JMenuItem attendanceItem = new JMenuItem("Attendance");
+        JMenuItem courseItem = new JMenuItem("Course Material");
+        JMenuItem quizRoomItem = new JMenuItem("Quiz Room");
+        JMenuItem contactItem = new JMenuItem("Contact");
+        JMenuItem logoutItem = new JMenuItem("Logout");
+
+        logoutItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Handle logout button click event
+                logout();
+            }
+        });
+
+        formatMenuItem(accountItem);
+        formatMenuItem(assessmentItem);
+        formatMenuItem(attendanceItem);
+        formatMenuItem(courseItem);
+        formatMenuItem(quizRoomItem);
+        formatMenuItem(contactItem);
+        formatMenuItem(logoutItem);
+
+        menuMenu.add(accountItem);
+        menuMenu.add(assessmentItem);
+        menuMenu.add(attendanceItem);
+        menuMenu.add(courseItem);
+        menuMenu.add(quizRoomItem);
+        menuMenu.add(contactItem);
+        menuMenu.add(logoutItem);
+
+        instructorMenuBar.add(homeMenu);
+        instructorMenuBar.add(menuMenu);
+
+        ImageIcon icon = createImageIcon("icon2.jpeg");
+        if (icon != null) {
+            this.setIconImage(icon.getImage());
+        }
+
+        this.setJMenuBar(instructorMenuBar);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
     private JPanel createPanel(String buttonText) {
         JPanel panel = new JPanel(new GridBagLayout());
 
@@ -78,6 +154,7 @@ public class InstructorHomePage extends Page {
 
         return panel;
     }
+
     private void formatButton(JButton button) {
         button.setFocusable(false);
         button.setFont(new Font("Arial", Font.BOLD, 18));
@@ -88,4 +165,11 @@ public class InstructorHomePage extends Page {
         menuItem.setFont(new Font("Arial", Font.PLAIN, 16));
         menuItem.setForeground(new Color(70, 130, 180));
     }
-}
+    private void account(){
+        new InstructorAccountPage();
+        this.dispose();
+    }
+    private void logout() {
+        new LoginPage();
+        this.dispose();
+    }
