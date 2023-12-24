425f00023bb6270dd34aa7316434716b9af2081a
Yafet Daniel <132993144+JaphD@users.noreply.github.com>
2023-12-24 20:38:48 +0300

    Update Main.java

--- EduTrack/src/GUI/Main.java
+++ EduTrack/src/GUI/Main.java
@@ -3,7 +3,5 @@ package GUI;
 public class Main {
     public static void main(String[] args) {
         new LoginPage();
-        //new StudentHomePage();
-        //new InstructorHomePage();
     }
 }
