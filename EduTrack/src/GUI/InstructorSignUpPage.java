86378d72d9e310e18a427d02d004a659f2b8ad9a
Yafet Daniel <132993144+JaphD@users.noreply.github.com>
2023-12-24 20:26:04 +0300

    Update InstructorSignUpPage.java

--- EduTrack/src/GUI/InstructorSignUpPage.java
+++ EduTrack/src/GUI/InstructorSignUpPage.java
@@ -8,10 +8,9 @@ import java.awt.event.ActionListener;
 
 public class InstructorSignUpPage extends Page implements ActionListener {
     private JLabel signUpLabel, nameLabel, departmentLabel, courseLabel, phoneNumberLabel, passwordLabel;
-    private JTextField nameField, departmentField, courseField, phoneNumberField, passwordField;
+    private JTextField firstNameField, lastNameField, userNameField, departmentField, courseField, phoneNumberField, passwordField;
     private JPanel signUpPanel, inputPanel;
     private JButton signUpButton;
-
     InstructorSignUpPage() {
         super("EduTrack - Instructor Sign Up", 800, 700, 211, 211, 211);
 
@@ -35,40 +34,42 @@ public class InstructorSignUpPage extends Page implements ActionListener {
         this.inputPanel = new JPanel(new GridBagLayout());
 
         // Name Label and TextField
-        addFormField("Name", nameField = new JTextField(20), 0);
+        addFormField("First name", firstNameField = new JTextField(20),0);
+
+        addFormField("Last name", lastNameField = new JTextField(20),1);
+
+        addFormField("Username", userNameField = new JTextField(20), 2);
 
         // Department Label and TextField
-        addFormField("Department", departmentField = new JTextField(20), 1);
+        addFormField("Department", departmentField = new JTextField(20), 3);
 
         // Course Label and TextField
-        addFormField("Course", courseField = new JTextField(20), 2);
+        addFormField("Course", courseField = new JTextField(20), 4);
 
         // PhoneNumber Label and TextField
-        addFormField("Phone Number", phoneNumberField = new JTextField(20), 3);
+        addFormField("Phone Number", phoneNumberField = new JTextField(20), 5);
 
         // Password Label and TextField
-        addFormField("Password", passwordField = new JPasswordField(20), 4);
+        addFormField("Password", passwordField = new JPasswordField(20), 6);
 
         GridBagConstraints constraints = new GridBagConstraints();
 
         // Sign Up Button
         this.signUpButton = new JButton("Sign Up");
         signUpButton.setFocusable(false);
-        signUpButton.setFocusable(false);
         signUpButton.setFont(new Font("Arial", Font.BOLD, 18));
         signUpButton.setForeground(Color.white);
         signUpButton.setBackground(new Color(70, 130, 180)); // Set color to a shade of blue
         signUpButton.addActionListener(this);
-        constraints.gridy = 5;
+        constraints.gridy = 8;
         constraints.gridx = 1;
         constraints.insets = new Insets(10, 0, 10, 0); // Add some spacing below the last text field
         inputPanel.add(signUpButton, constraints);
 
-        frame.add(signUpPanel, BorderLayout.NORTH);
-        frame.add(inputPanel, BorderLayout.CENTER);
-        frame.setVisible(true);
+        page.add(signUpPanel, BorderLayout.NORTH);
+        page.add(inputPanel, BorderLayout.CENTER);
+        page.setVisible(true);
     }
-
     private void addFormField(String labelText, JComponent field, int gridY) {
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridy = gridY;
