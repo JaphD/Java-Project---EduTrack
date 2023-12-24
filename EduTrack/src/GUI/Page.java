dc93c98a94724ed0926fb2002ba3e4ac9f1ea50f
Yafet Daniel <132993144+JaphD@users.noreply.github.com>
2023-12-24 20:18:54 +0300

    Update Page.java

--- EduTrack/src/GUI/Page.java
+++ EduTrack/src/GUI/Page.java
@@ -3,15 +3,16 @@ package GUI;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
+import java.net.URL;
 import javax.swing.ImageIcon;
 
-abstract class Page extends JFrame{
-    protected JFrame frame;
+abstract class Page extends JFrame {
+    protected JFrame page;
     private String title;
     private int backgroundColorRed, backgroundColorGreen, backgroundColorBlue;
     private int width, height;
-    Page(String title,int width,int height, int backgroundColorRed, int backgroundColorGreen,int backgroundColorBlue) {
-        this.frame = new JFrame(title);
+    Page(String title, int width, int height, int backgroundColorRed, int backgroundColorGreen, int backgroundColorBlue) {
+        this.page = new JFrame(title);
         this.title = title;
         this.width = width;
         this.height = height;
@@ -19,35 +20,46 @@ abstract class Page extends JFrame{
         this.backgroundColorGreen = backgroundColorGreen;
         this.backgroundColorBlue = backgroundColorBlue;
 
-        initializeFrame(false);
+        initializePage(false);
     }
-    Page(String title, int backgroundColorRed, int backgroundColorGreen, int backgroundColorBlue)  {
-        this.frame = new JFrame(title);
+    Page(String title, int backgroundColorRed, int backgroundColorGreen, int backgroundColorBlue) {
+        this.page = new JFrame(title);
         this.title = title;
         this.backgroundColorRed = backgroundColorRed;
         this.backgroundColorGreen = backgroundColorGreen;
         this.backgroundColorBlue = backgroundColorBlue;
 
         setExtendedState();
-        initializeFrame(true);
-        }
-    private void initializeFrame(boolean resizable){
-        frame.setTitle(title);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setSize(width, height);
-        frame.setLocationRelativeTo(null); // to center on the screen
-        frame.getContentPane().setBackground(new Color(backgroundColorRed, backgroundColorGreen, backgroundColorBlue));
-        frame.setResizable(resizable);
-
-        ImageIcon icon = new ImageIcon("C:\\Users\\Japhe\\OneDrive\\Desktop\\Java\\EduTrack\\src\\GUI\\icon2.jpeg");
-        frame.setIconImage(icon.getImage());
+        initializePage(true);
     }
-    private void setExtendedState(){
-            setSize(Toolkit.getDefaultToolkit().getScreenSize());
-            setExtendedState(JFrame.MAXIMIZED_BOTH);
+    private void initializePage(boolean resizable) {
+        page.setTitle(title);
+        page.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        page.setLayout(new BorderLayout());
+        page.setSize(width, height);
+        page.setLocationRelativeTo(null); // to center on the screen
+        page.getContentPane().setBackground(new Color(backgroundColorRed, backgroundColorGreen, backgroundColorBlue));
+        page.setResizable(resizable);
+        
+        ImageIcon icon = createImageIcon("icon2.jpeg");
+        if (icon != null) {
+            page.setIconImage(icon.getImage());
         }
     }
+    private void setExtendedState() {
+        setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+    protected ImageIcon createImageIcon(String filename){
+        URL imgUrl = getClass().getClassLoader().getResource(filename);
+        if (imgUrl != null) {
+            return new ImageIcon(imgUrl);
+        } else {
+            System.err.println("Couldn't find file: " + filename);
+            return null;
+        }
+    }
+}
 
 
 
